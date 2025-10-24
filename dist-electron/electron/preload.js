7768d71e60c6d3ff18fb95e6769d2803f4434fd5
Shrikar <69652900+ShrikarSwami@users.noreply.github.com>
2025-10-24 15:48:02 -0400

    Implement code changes to enhance functionality and improve performance

--- dist-electron/electron/preload.js
+++ dist-electron/electron/preload.js
@@ -1,16 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
-// Minimal API used by the React app
 electron_1.contextBridge.exposeInMainWorld('api', {
     getSettings: () => electron_1.ipcRenderer.invoke('settings:get'),
     updateSettings: (payload) => electron_1.ipcRenderer.invoke('settings:update', payload),
-    setSecret: (key, value) => electron_1.ipcRenderer.invoke('secrets:set', { key, value }),
-    getSecret: (key) => electron_1.ipcRenderer.invoke('secrets:get', key)
-});
-electron_1.contextBridge.exposeInMainWorld('api', {
-    getSettings: () => electron_1.ipcRenderer.invoke('settings:get'),
-    updateSettings: (payload) => electron_1.ipcRenderer.invoke('settings:update', payload),
+    // keychain helpers
     setSecret: (key, value) => electron_1.ipcRenderer.invoke('secrets:set', { key, value }),
     getSecret: (key) => electron_1.ipcRenderer.invoke('secrets:get', key),
 });
