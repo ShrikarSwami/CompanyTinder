32a422a342fc21f152ed41b253eee85b2c741530
Shrikar <69652900+ShrikarSwami@users.noreply.github.com>
2025-10-25 19:00:22 -0400

    Updating file directories

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: { port: 5173 }
 })
+
+build: {
+  outDir: 'dist-electron',   // Ensure Vite builds to dist-electron
+  rollupOptions: {
+    input: 'src/index.html'  // Specify the path to index.html in the src folder
+  }
+}
